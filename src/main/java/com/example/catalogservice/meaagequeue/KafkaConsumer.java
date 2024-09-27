12e7e9337fd41f4007a9b2890c0aeeaed444dfde
신희성 <vvwv3092@gmail.com>
2024-09-27 11:08:24 +0900

    add: kafka consumer

---
+++ src/main/java/com/example/catalogservice/meaagequeue/KafkaConsumer.java
@@ -0,0 +1,42 @@
+package com.example.catalogservice.meaagequeue;
+
+import com.example.catalogservice.entity.CatalogEntity;
+import com.example.catalogservice.repository.CatalogRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class KafkaConsumer {
+    private final CatalogRepository catalogRepository;
+
+    @KafkaListener(topics = "example-catalog-topic")
+    public void updateQty(String kafkaMessage) {
+        log.info("Kafka Message: -> {}", kafkaMessage);
+
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {
+            });
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        CatalogEntity catalogEntity = catalogRepository.findByProductId((String) map.get("productId"));
+        catalogEntity.setStock(catalogEntity.getStock() - (Integer) map.get("qty"));
+
+        catalogRepository.save(catalogEntity);
+    }
+
+}
